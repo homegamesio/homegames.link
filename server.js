db94fd27eb017b1cc5a9a839d1eee99a43e74e7c
Joseph Garcia <prosif@gmail.com>
2019-02-03 20:03:31 -0800

    wip

--- server.js
+++ server.js
@@ -2,18 +2,45 @@ const WebSocket = require('ws');
 const http = require('http');
 const fs = require('fs');
 
-const PORT = 7000;
+const PORT = 80;
 
-const server = http.createServer();
+// if this thing becomes popular, this will never work. until then, it's fine.
+let id = 0;
+
+const hostMap = {};
+
+const server = http.createServer((req, res) => {
+	const requesterIp = req.connection.remoteAddress;
+	if (hostMap[requesterIp]) {
+	    res.write(hostMap[requesterIp]);
+	} else {
+	    res.write('none');
+	}
+	res.end();
+});;
 
 const wss = new WebSocket.Server({
     server
 });
 
+const clients = {};
 wss.on('connection', (ws, req) => {
-    console.log("Someone connected");
-    console.log(req.connection.remoteAddress);
+	ws.id = id++;
+	clients[ws.id] = ws;
+
+        const networkIp = req.connection.remoteAddress;
+
+	ws.on('message', (ip) => {
+            hostMap[networkIp] = ip;
+	});
+
+        ws.on('close', () => {
+            delete hostmap[networkIp]; 
+            delete clients[ws.id];
+        });
 });
 
-server.listen(PORT, () => {});
+server.listen(PORT, () => {
+	console.log("listening");
+});
 
