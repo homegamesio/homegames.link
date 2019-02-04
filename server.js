b9f78f6c7b6f4bb2824443b60888b1b014656625
Joseph Garcia <prosif@gmail.com>
2019-02-03 21:26:06 -0800

    wip

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
 const WebSocket = require('ws');
-const http = require('http');
-const fs = require('fs');
 
-const PORT = 80;
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const app = express();
+
+const privateKey = fs.readFileSync('ssl/localhost.key');
+const certificate = fs.readFileSync('ssl/localhost.crt');
+
+const ensureSecure = (req, res, next) => {
+    if (req.secure) {
+        return next();
+    }
+
+    res.redirect('https://' + req.hostname + req.url);
+}
+
+app.all('*', ensureSecure);
+
+const hostMap = {};
+
+app.get('/', (req, res) => {
+    const requesterIp = req.connection.remoteAddress;
+    console.log(requesterIp);
+    console.log(hostMap);
+    if (hostMap[requesterIp]) {
+        res.write(hostMap[requesterIp]);
+    } else {
+        res.write('none');
+    }
+
+    res.end();
+});
+
+const httpServer = http.createServer(app);
+
+const server = https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app);
+
+const PORT = 7000;
 
 // if this thing becomes popular, this will never work. until then, it's fine.
 let id = 0;
 
-const hostMap = {};
 
-const server = http.createServer((req, res) => {
-	const requesterIp = req.connection.remoteAddress;
-	if (hostMap[requesterIp]) {
-	    res.write(hostMap[requesterIp]);
-	} else {
-	    res.write('none');
-	}
-	res.end();
-});;
-
-const wss = new WebSocket.Server({
-    server
-});
+const wss = new WebSocket.Server({port: PORT});
 
 const clients = {};
 wss.on('connection', (ws, req) => {
@@ -35,12 +61,11 @@ wss.on('connection', (ws, req) => {
 	});
 
         ws.on('close', () => {
-            delete hostmap[networkIp]; 
+            delete hostMap[networkIp]; 
             delete clients[ws.id];
         });
 });
 
-server.listen(PORT, () => {
-	console.log("listening");
-});
+httpServer.listen(80);
+server.listen(443);
 
