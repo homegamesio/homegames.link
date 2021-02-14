e428d7624cd294c58541ebb1995943810e350aac
Joseph Garcia <prosif@gmail.com>
2021-02-13 23:56:39 -0700

    more

--- server.js
+++ server.js
@@ -7,59 +7,58 @@ const AWS = require('aws-sdk');
 const redis = require('redis');
 const { v4: uuidv4 } = require('uuid');
 
-//const wsServer = http.createServer();
-////
-////
-//// todo: move to common
-//const createDNSRecord = (url, ip) => new Promise((resolve, reject) => {
-//    const params = {
-//        ChangeBatch: {
-//            Changes: [
-//                {
-//                    Action: "CREATE", 
-//                    ResourceRecordSet: {
-//                        Name: url,
-//                        ResourceRecords: [
-//                            {
-//                                Value: ip
-//                            }
-//                        ], 
-//                        TTL: 60, 
-//                        Type: "A"
-//                    }
-//                }
-//            ]
-//        }, 
-//        HostedZoneId: process.env.AWS_ROUTE_53_HOSTED_ZONE_ID
-//    };
-//
-//    const route53 = new AWS.Route53();
-//    
-//    route53.changeResourceRecordSets(params, (err, data) => {
-//	    resolve();
-//    });
-//});
-//
-//const verifyDNSRecord = (url, ip) => new Promise((resolve, reject) => {
-//    const route53 = new AWS.Route53();
-//
-//    const params = {
-//        HostedZoneId: process.env.AWS_ROUTE_53_HOSTED_ZONE_ID,
-//        StartRecordName: url,
-//        StartRecordType: 'A',
-//	MaxItems: '1'
-//    };
-//    
-//    route53.listResourceRecordSets(params, (err, data) => {
-//	    if (data.ResourceRecordSets.length === 0 || data.ResourceRecordSets[0].Name !== url) {
-//		    createDNSRecord(url, ip).then(() => {
-//	    		resolve();
-//		    });
-//	    } else {
-//	    	resolve();
-//	    }
-//    });
-//});
+const wsServer = http.createServer();
+
+// todo: move to common
+const createDNSRecord = (url, ip) => new Promise((resolve, reject) => {
+    const params = {
+        ChangeBatch: {
+            Changes: [
+                {
+                    Action: "CREATE", 
+                    ResourceRecordSet: {
+                        Name: url,
+                        ResourceRecords: [
+                            {
+                                Value: ip
+                            }
+                        ], 
+                        TTL: 60, 
+                        Type: "A"
+                    }
+                }
+            ]
+        }, 
+        HostedZoneId: process.env.AWS_ROUTE_53_HOSTED_ZONE_ID
+    };
+
+    const route53 = new AWS.Route53();
+    
+    route53.changeResourceRecordSets(params, (err, data) => {
+	    resolve();
+    });
+});
+
+const verifyDNSRecord = (url, ip) => new Promise((resolve, reject) => {
+    const route53 = new AWS.Route53();
+
+    const params = {
+        HostedZoneId: process.env.AWS_ROUTE_53_HOSTED_ZONE_ID,
+        StartRecordName: url,
+        StartRecordType: 'A',
+	MaxItems: '1'
+    };
+    
+    route53.listResourceRecordSets(params, (err, data) => {
+	    if (data.ResourceRecordSets.length === 0 || data.ResourceRecordSets[0].Name !== url) {
+		    createDNSRecord(url, ip).then(() => {
+	    		resolve();
+		    });
+	    } else {
+	    	resolve();
+	    }
+    });
+});
 
 const redisClient = () => {
 	return redis.createClient({
@@ -67,115 +66,112 @@ const redisClient = () => {
 		port: process.env.REDIS_PORT
 	});
 };
-//
-//const redisGet = (key) => new Promise((resolve, reject) => {
-//	const client = redisClient();
-//
-//	client.get(key, (err, res) => {
-//		if (err) {
-//			reject(err);
-//		} else {
-//			resolve(res);
-//		}
-//	});
-//});
-//
-//const redisSet = (key, value) => new Promise((resolve, reject) => {	
-//	const client = redisClient();
-//
-//	client.set(key, value, (err, res) => {
-//		if (err) {
-//			reject(err);
-//		} else {
-//			resolve(res);
-//		}
-//	});
-//
-//});
-//
-//const redisHmset = (key, obj) => new Promise((resolve, reject) => {
-//	const client = redisClient();
-//
-//	client.get(key, (err, res) => {
-//		if (err) {
-//			reject(err);
-//		} else {
-//			resolve(res);
-//		}
-//	});
-//});
-//
-//const getHostInfo = (publicIp, serverId) => new Promise((resolve, reject) => {
-//	const client = redisClient();
-//
-//	client.hmget(publicIp, [serverId], (err, data) => {
-//		if (err || !data) {
-//			reject(err || 'No host data found');
-//		} else {
-//			resolve(data[0]);
-//		}
-//	});
-//
-//});
 
-//const app = (req, res) => {
-//	const requesterIp = req.connection.remoteAddress;
-//        
-//
-//	const noServers = () => {
-//		res.writeHead(200, {
-//			'Content-Type': 'text/plain'
-//		});
-//		res.end('No Homegames servers found. Contact support@homegames.io for help hello ');// + requesterIp + ' ' + process.env.REDIS_HOST + ':' + process.end.REDIS_PORT);
-//	};
-//        console.log(requesterIp);
-//        noServers();
-//
-//	getHomegamesServers(requesterIp).then(servers => {
-//		const serverIds = servers && Object.keys(servers) || [];
-//		if (serverIds.length === 1) {
-//			const serverInfo = JSON.parse(servers[serverIds[0]]);
-//			const hasHttps = serverInfo.https;
-//			const prefix = hasHttps ? 'https' : 'http';
-//			const urlOrIp = serverInfo.verifiedUrl || serverInfo.localIp;
-//			res.writeHead(307, {
-//				'Location': `${prefix}://${urlOrIp}`,
-//				'Cache-Control': 'no-store'
-//			});
-//			res.end();
-//		} else if (serverIds.length > 1) {
-//			const serverOptions = serverIds.map(serverId => {
-//				const serverInfo = JSON.parse(servers[serverId]);
-//
-//				const prefix = serverInfo.https ? 'https': 'http';
-//				const urlOrIp = serverInfo.verifiedUrl || serverInfo.localIp;
-//				const lastHeartbeat = new Date(Number(serverInfo.timestamp));
-//				return `<li><a href="${prefix}://${urlOrIp}"}>Server ID: ${serverId} (Last heartbeat: ${lastHeartbeat})</a></li>`
-//			});
-//
-//			const content = `Homegames server selector: <ul>${serverOptions.join('')}</ul>`;
-//			const response = `<html><body>${content}</body></html>`;
-//			res.writeHead(200, {
-//				'Content-Type': 'text/html'
-//			});
-//			res.end(response);
-//		} else {
-//			console.log('no servers');
-//			noServers();
-//		}
-//	}).catch(err => {
-//		console.log('Error getting host info');
-//		console.log(err);
-//		noServers();
-//	});
-//};
+const redisGet = (key) => new Promise((resolve, reject) => {
+	const client = redisClient();
+
+	client.get(key, (err, res) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve(res);
+		}
+	});
+});
+
+const redisSet = (key, value) => new Promise((resolve, reject) => {	
+	const client = redisClient();
+
+	client.set(key, value, (err, res) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve(res);
+		}
+	});
+
+});
+
+const redisHmset = (key, obj) => new Promise((resolve, reject) => {
+	const client = redisClient();
+
+	client.get(key, (err, res) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve(res);
+		}
+	});
+});
+
+const getHostInfo = (publicIp, serverId) => new Promise((resolve, reject) => {
+	const client = redisClient();
+
+	client.hmget(publicIp, [serverId], (err, data) => {
+		if (err || !data) {
+			reject(err || 'No host data found');
+		} else {
+			resolve(data[0]);
+		}
+	});
+
+});
+
+const app = (req, res) => {
+	const requesterIp = req.connection.remoteAddress;
+        
+
+	const noServers = () => {
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+		res.end('No Homegames servers found. Contact support@homegames.io for help hello ');// + requesterIp + ' ' + process.env.REDIS_HOST + ':' + process.end.REDIS_PORT);
+	};
+        console.log(requesterIp);
+        noServers();
+
+	getHomegamesServers(requesterIp).then(servers => {
+		const serverIds = servers && Object.keys(servers) || [];
+		if (serverIds.length === 1) {
+			const serverInfo = JSON.parse(servers[serverIds[0]]);
+			const hasHttps = serverInfo.https;
+			const prefix = hasHttps ? 'https' : 'http';
+			const urlOrIp = serverInfo.verifiedUrl || serverInfo.localIp;
+			res.writeHead(307, {
+				'Location': `${prefix}://${urlOrIp}`,
+				'Cache-Control': 'no-store'
+			});
+			res.end();
+		} else if (serverIds.length > 1) {
+			const serverOptions = serverIds.map(serverId => {
+				const serverInfo = JSON.parse(servers[serverId]);
+
+				const prefix = serverInfo.https ? 'https': 'http';
+				const urlOrIp = serverInfo.verifiedUrl || serverInfo.localIp;
+				const lastHeartbeat = new Date(Number(serverInfo.timestamp));
+				return `<li><a href="${prefix}://${urlOrIp}"}>Server ID: ${serverId} (Last heartbeat: ${lastHeartbeat})</a></li>`
+			});
+
+			const content = `Homegames server selector: <ul>${serverOptions.join('')}</ul>`;
+			const response = `<html><body>${content}</body></html>`;
+			res.writeHead(200, {
+				'Content-Type': 'text/html'
+			});
+			res.end(response);
+		} else {
+			console.log('no servers');
+			noServers();
+		}
+	}).catch(err => {
+		console.log('Error getting host info');
+		console.log(err);
+		noServers();
+	});
+};
 
 const hostMapServer = http.createServer((req, res) => {
-//		host: process.env.REDIS_HOST,
-//		port: process.env.REDIS_PORT
-
     const client = redisClient();
-    res.end('ok!! ' + process.env.REDIS_HOST + ' : ' + process.env.REDIS_PORT);
+    res.end('ok!111! ' + process.env.REDIS_HOST + ' : ' + process.env.REDIS_PORT);
 });
 
 //const wss = new WebSocket.Server({server: wsServer});
