888310c6aa312780fabd4eb8ad5eec895929fa48
Joseph Garcia <prosif@gmail.com>
2021-02-14 00:01:14 -0700

    mas

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const AWS = require('aws-sdk');
 const redis = require('redis');
 const { v4: uuidv4 } = require('uuid');
 
-const wsServer = http.createServer();
-
 // todo: move to common
 const createDNSRecord = (url, ip) => new Promise((resolve, reject) => {
     const params = {
@@ -171,172 +169,171 @@ const app = (req, res) => {
 
 const hostMapServer = http.createServer((req, res) => {
     const client = redisClient();
-    res.end('ok!111! ' + process.env.REDIS_HOST + ' : ' + process.env.REDIS_PORT);
+    res.end('ok!1211! ' + process.env.REDIS_HOST + ' : ' + process.env.REDIS_PORT);
 });
 
-//const wss = new WebSocket.Server({server: wsServer});
-//
-//const clients = {};
-//
-//
-//// Redis key structure
-////{
-////  "publicIp": {
-////    "serverId1": {
-////      ...
-////    },
-////    "serverId2": {
-////	...
-////    },
-////    ...
-////  }
-////}
-//const getHomegamesServers = (publicIp) => new Promise((resolve, reject) => {
-//	const client = redisClient();
-//
-//	client.hgetall(publicIp, (err, data) => {
-//		if (err) {
-//			reject(err);
-//		} else {
-//			resolve(data);
-//		}
-//	});
-//});
-//
-//const deleteHostInfo = (publicIp, localIp) => new Promise((resolve, reject) => {
-//	const client = redisClient();
-//
-//	client.hdel(publicIp, [localIp], (err, data) => {
-//		if (err) {
-//			reject(err);
-//		} else {
-//			resolve();
-//		}
-//	});
-//});
-//
-//const registerHost = (publicIp, info, hostId) => new Promise((resolve, reject) => {
-//	const client = redisClient();
-//
-//	const doUpdate = () => {
-//		const payload = Object.assign({}, info);
-//		payload.timestamp = Date.now();
-//		client.hmset(publicIp, [hostId, JSON.stringify(payload)], (err, data) => {
-//			if (err) {
-//				reject(err);
-//			} else {
-//				resolve();
-//			}
-//		});
-//	}
-//
-//	// clear out existing entries
-//	client.hgetall(publicIp, (err, data) => {
-//		const idsToRemove = [];
-//		for (serverId in data) {
-//			const serverInfo = JSON.parse(data[serverId]);
-//			if (serverInfo.localIp && serverInfo.localIp === info.localIp || !serverInfo.timestamp || serverInfo.timestamp + (5 * 1000 * 60) <= Date.now()) {
-//				idsToRemove.push(serverId);
-//			}
-//		}
-//
-//		let toDeleteCount = idsToRemove.length;
-//
-//		if (toDeleteCount === 0) {
-//			doUpdate();
-//		} else {
-//
-//			for (const idIndex in idsToRemove) {
-//				const id = idsToRemove[idIndex];
-//
-//				client.hdel(publicIp, [id], (err, data) => {
-//					toDeleteCount -= 1;
-//					if (toDeleteCount == 0) {
-//						doUpdate();
-//					}
-//				});
-//			}
-//		}
-//	});
-//});
-//
-//const generateSocketId = () => {
-//	return uuidv4();
-//};
-//
-//const updatePresence = (publicIp, serverId) => {
-//	getHostInfo(publicIp, serverId).then(hostInfo => {
-//		if (!hostInfo) {
-//			return;
-//		}
-//		registerHost(publicIp, JSON.parse(hostInfo), serverId).then(() => {
-//			console.log('updated presence');
-//		});
-//	});
-//};
-//
-//const updateHostInfo = (publicIp, serverId, update) => new Promise((resolve, reject) => {
-//	getHostInfo(publicIp, serverId).then(hostInfo => {
-//		const newInfo = Object.assign(JSON.parse(hostInfo), update);
-//		registerHost(publicIp, newInfo, serverId).then(() => {
-//			console.log('updated data');
-//		});
-//	});
-//});
-//
-//wss.on('connection', (ws, req) => {
-//	const socketId = generateSocketId();
-//	ws.id = generateSocketId();
-//	clients[ws.id] = ws;
-//
-//        const publicIp = req.connection.remoteAddress;
-//
-//	ws.on('message', (_message) => {
-//	   
-//		try {
-//            		const message = JSON.parse(_message);
-//
-//			if (message.type === 'heartbeat') {
-//				updatePresence(publicIp, ws.id);
-//			} else if (message.type === 'register') {
-//				registerHost(publicIp, message.data, ws.id).then(() => {
-//					console.log('registered host');
-//				});
-//	    		} else if (message.type === 'verify-dns') {
-//				verifyAccessToken(message.username, message.accessToken).then(() => {
-//		    			const ipSub = message.localIp.replace(/\./g, '-');
-//					const userHash = getUserHash(message.username);
-//		    			const userUrl = `${ipSub}.${userHash}.homegames.link`;
-//					verifyDNSRecord(userUrl, message.localIp).then(() => {
-//						ws.send(JSON.stringify({
-//							msgId: message.msgId,
-//							url: userUrl,
-//							success: true
-//						}));
-//						updateHostInfo(publicIp, ws.id, {verifiedUrl: userUrl});
-//					});
-//				});
-//			} else {
-//	    		    console.log("received message without ip");
-//	    		    console.log(message);
-//	    		}
-//		} catch (err) {
-//			console.log("Error processing client message");
-//			console.error(err);
-//		}
-//
-//	});
-//
-//        ws.on('close', () => {
-//            delete clients[ws.id];
-//
-//		deleteHostInfo(publicIp, ws.id).then(() => {
-//			console.log('deleetedede');
-//		});
-//        });
-//});
+const wss = new WebSocket.Server({server: hostMapServer });
+
+const clients = {};
+
+
+// Redis key structure
+//{
+//  "publicIp": {
+//    "serverId1": {
+//      ...
+//    },
+//    "serverId2": {
+//	...
+//    },
+//    ...
+//  }
+//}
+const getHomegamesServers = (publicIp) => new Promise((resolve, reject) => {
+	const client = redisClient();
+
+	client.hgetall(publicIp, (err, data) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve(data);
+		}
+	});
+});
+
+const deleteHostInfo = (publicIp, localIp) => new Promise((resolve, reject) => {
+	const client = redisClient();
+
+	client.hdel(publicIp, [localIp], (err, data) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve();
+		}
+	});
+});
+
+const registerHost = (publicIp, info, hostId) => new Promise((resolve, reject) => {
+	const client = redisClient();
+
+	const doUpdate = () => {
+		const payload = Object.assign({}, info);
+		payload.timestamp = Date.now();
+		client.hmset(publicIp, [hostId, JSON.stringify(payload)], (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	}
+
+	// clear out existing entries
+	client.hgetall(publicIp, (err, data) => {
+		const idsToRemove = [];
+		for (serverId in data) {
+			const serverInfo = JSON.parse(data[serverId]);
+			if (serverInfo.localIp && serverInfo.localIp === info.localIp || !serverInfo.timestamp || serverInfo.timestamp + (5 * 1000 * 60) <= Date.now()) {
+				idsToRemove.push(serverId);
+			}
+		}
+
+		let toDeleteCount = idsToRemove.length;
+
+		if (toDeleteCount === 0) {
+			doUpdate();
+		} else {
+
+			for (const idIndex in idsToRemove) {
+				const id = idsToRemove[idIndex];
+
+				client.hdel(publicIp, [id], (err, data) => {
+					toDeleteCount -= 1;
+					if (toDeleteCount == 0) {
+						doUpdate();
+					}
+				});
+			}
+		}
+	});
+});
+
+const generateSocketId = () => {
+	return uuidv4();
+};
+
+const updatePresence = (publicIp, serverId) => {
+	getHostInfo(publicIp, serverId).then(hostInfo => {
+		if (!hostInfo) {
+			return;
+		}
+		registerHost(publicIp, JSON.parse(hostInfo), serverId).then(() => {
+			console.log('updated presence');
+		});
+	});
+};
+
+const updateHostInfo = (publicIp, serverId, update) => new Promise((resolve, reject) => {
+	getHostInfo(publicIp, serverId).then(hostInfo => {
+		const newInfo = Object.assign(JSON.parse(hostInfo), update);
+		registerHost(publicIp, newInfo, serverId).then(() => {
+			console.log('updated data');
+		});
+	});
+});
+
+wss.on('connection', (ws, req) => {
+	const socketId = generateSocketId();
+	ws.id = generateSocketId();
+	clients[ws.id] = ws;
+
+        const publicIp = req.connection.remoteAddress;
+
+	ws.on('message', (_message) => {
+	   
+		try {
+            		const message = JSON.parse(_message);
+
+			if (message.type === 'heartbeat') {
+				updatePresence(publicIp, ws.id);
+			} else if (message.type === 'register') {
+				registerHost(publicIp, message.data, ws.id).then(() => {
+					console.log('registered host');
+				});
+	    		} else if (message.type === 'verify-dns') {
+				verifyAccessToken(message.username, message.accessToken).then(() => {
+		    			const ipSub = message.localIp.replace(/\./g, '-');
+					const userHash = getUserHash(message.username);
+		    			const userUrl = `${ipSub}.${userHash}.homegames.link`;
+					verifyDNSRecord(userUrl, message.localIp).then(() => {
+						ws.send(JSON.stringify({
+							msgId: message.msgId,
+							url: userUrl,
+							success: true
+						}));
+						updateHostInfo(publicIp, ws.id, {verifiedUrl: userUrl});
+					});
+				});
+			} else {
+	    		    console.log("received message without ip");
+	    		    console.log(message);
+	    		}
+		} catch (err) {
+			console.log("Error processing client message");
+			console.error(err);
+		}
+
+	});
+
+        ws.on('close', () => {
+            delete clients[ws.id];
+
+		deleteHostInfo(publicIp, ws.id).then(() => {
+			console.log('deleetedede');
+		});
+        });
+});
 
 hostMapServer.listen(80);
-//wsServer.listen(7080);
 //
 //const HTTP_PORT = 80;
